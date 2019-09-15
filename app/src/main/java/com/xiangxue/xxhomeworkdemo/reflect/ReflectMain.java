b2557aced4abee96e938757b9c6dd5672a7a2382
lwb <18614005205@163.com>
2019-09-15 22:42:10 +0800

    显示锁/ 非/可 重入锁 公平锁/非公平锁 读/写 锁
    笔记

--- app/src/main/java/com/xiangxue/xxhomeworkdemo/reflect/ReflectMain.java
+++ app/src/main/java/com/xiangxue/xxhomeworkdemo/reflect/ReflectMain.java
@@ -1,6 +1,14 @@
 package com.xiangxue.xxhomeworkdemo.reflect;
 
 public class ReflectMain {
+
+    private static ThreadLocal<Integer> local = new ThreadLocal<Integer>(){
+        @Override
+        protected Integer initialValue() {
+            return 0;
+        }
+    };
+
     public static void main(String[] args) {
         LwbCompany lwbCompany = new LwbCompany();  // 创建代理类
         TicketsService service = new TicketsServiceImpl();
