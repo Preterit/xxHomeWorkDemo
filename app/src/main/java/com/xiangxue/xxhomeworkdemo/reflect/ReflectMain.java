399f27485b44c58365ba05f2c6ea77807b9c3d81
liwenbo <18614005205@163.com>
2019-09-16 18:34:45 +0800

    9/16

--- app/src/main/java/com/xiangxue/xxhomeworkdemo/reflect/ReflectMain.java
+++ app/src/main/java/com/xiangxue/xxhomeworkdemo/reflect/ReflectMain.java
@@ -1,11 +1,26 @@
 package com.xiangxue.xxhomeworkdemo.reflect;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
 public class ReflectMain {
 
-    private static ThreadLocal<Integer> local = new ThreadLocal<Integer>(){
+    /**
+     * ThreadLocal 线程隔离
+     * 使用情景: 当线程需要操作上一个线程修改的完的数据,为了保证数据的准确性,
+     */
+    private static ThreadLocal<Integer> local = new ThreadLocal<Integer>() {
         @Override
         protected Integer initialValue() {
             return 0;
+        }
+    };
+
+    @SuppressLint("StaticFieldLeak")
+    private static AsyncTask task = new AsyncTask() {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
         }
     };
 
@@ -16,6 +31,7 @@ public class ReflectMain {
 
         TicketsService proxyInstance = (TicketsService) lwbCompany.getProxyInstance();
         proxyInstance.inquire();
+        task.execute();
     }
 }
 
