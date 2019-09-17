8e070c95553f45f5b038ad4321cdee1cc153c73c
lwb <18614005205@163.com>
2019-09-17 23:51:20 +0800

    io操作

---
+++ app/src/main/java/com/xiangxue/xxhomeworkdemo/io/JavaIO.java
@@ -0,0 +1,35 @@
+package com.xiangxue.xxhomeworkdemo.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Date:2019-09-17
+ * author:lwb
+ * Desc:
+ */
+public class JavaIO {
+    static String filePath = "/Users/liwenbo/Desktop/oop";
+    public static void main(String[] agrs) throws Exception {
+        System.out.println("sssss");
+//        File file = new File(filePath);
+//        writeFile("你好sjljflsjflsjlfjlsfdsdf滚蛋吧", "1.txt");
+    }
+
+    private static void writeFile(String content, String fileName) throws Exception {
+        byte[] bytes = content.getBytes();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath + "/"+fileName));
+
+        int len = -1;
+        byte[] buffer = new byte[1024 * 100];
+        while ((len = byteArrayInputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, len);
+        }
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        byteArrayInputStream.close();
+        new String(buffer);
+    }
+}
